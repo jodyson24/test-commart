b583c9d5464ab244d509c8f7da8ef281d289cf6f
jodyson24 <joshua.adegbola20@gmail.com>
2020-10-29 22:06:34 +0100

    Update PaypalButton.js
    Rollback

--- client/src/components/mainpages/cart/PaypalButton.js
+++ client/src/components/mainpages/cart/PaypalButton.js
@@ -29,7 +29,7 @@ export default class PaypalButton extends React.Component {
         // Document on Paypal's currency code: https://developer.paypal.com/docs/classic/api/currency_codes/
 
         const client = {
-            sandbox: 'YOUR-sandbox-APP-ID',
+            sandbox: 'AQVDJbL0QudgAh227fq5dSSYslwDKdqSotXbPCZEeNWc2SBlzXQZzT-lKJbHqQRpRCI0izSXr6yscqUD',
             production: 'YOUR-PRODUCTION-APP-ID',
         }
         // In order to get production's app-ID, you will have to send your app to Paypal for approval first
