c368897d560311b00372f7b63893e5fd3d2b780e
J Ade <joshua.adegbola20@gmail.com>
2024-06-20 16:21:11 +0100

    Update server.js
    Changed the and bound the port

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ if(process.env.NODE_ENV === 'production'){
     })
 }
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => {
-	console.log('Server is running on port', PORT)
+const port = process.env.PORT || 5450
+app.listen(port, () => {
+	console.log('Server is running on port', ${port})
 })
